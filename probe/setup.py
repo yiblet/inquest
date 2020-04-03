704264be36ac17191fd2b6ded22cef6f0aeb6cd9
Shalom Yiblet <shalom.yiblet@gmail.com>
2020-04-03 15:54:25 -0400

    feat(probe): fix dependency issues

--- probe/setup.py
+++ probe/setup.py
@@ -1,7 +1,17 @@
 import pathlib
+import re
+
 from setuptools import setup
 
-REQUIREMENTS = pathlib.Path('requirements.txt').open().readlines()
+REGEX = re.compile(r'^[\w-]*(==\w*\s*)?$')
+
+REQUIREMENTS = [
+    statement
+    for statement in pathlib.Path('requirements.txt').open().readlines()
+    if REGEX.match(statement)
+]
+
+print(REQUIREMENTS)
 
 setup(
     name='inquest',
@@ -9,8 +19,9 @@ setup(
     description='the inquest logger',
     author='Shalom Yiblet',
     author_email='shalom.yiblet@gmail.com',
-    # TODO make this more robust to changes in the requirements.txt
-    install_requires=REQUIREMENTS[:-1],
-    dependency_links=REQUIREMENTS[-1:],
+    install_requires=REQUIREMENTS + ['gql'],
+    dependency_links=[
+        'http://github.com/leszekhanusz/gql/tarball/ada2d6ce50388a714fe9cbea89f5f642e0e5f2e0#egg=gql-0.4.0'
+    ],
     packages=['inquest'],
 )
