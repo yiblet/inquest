a3f9eeed1e3413f5faa5883de6e5b50df085b451
Shalom Yiblet <shalom.yiblet@gmail.com>
2020-06-12 13:14:44 -0400

    feat(probe): prevent multiple inquest instances from running at the same time

--- probe/inquest/runner.py
+++ probe/inquest/runner.py
@@ -15,6 +15,9 @@ from inquest.probe import Probe
 
 LOGGER = logging.getLogger(__name__)
 
+_LOCK = threading.Lock()
+_ENABLED = False
+
 
 # TODO split out logic of probe runner to only be connecting together 1 or 2
 #      existing services rather than running the logic itself
@@ -75,14 +78,25 @@ class ProbeRunner(threading.Thread):
         return consumers
 
     def run(self):
-        LOGGER.info('inquest daemon is running')
-        evloop = asyncio.new_event_loop()
-        evloop.run_until_complete(self._run_async())
-        LOGGER.info('inquest daemon closed')
+        """
+        The ProbeRunner's main loop. This fires up the full inquest probe.
+        It starts communicating with the backend in a background thread.
+        """
+        global _ENABLED
+        try:
+            LOGGER.info('inquest daemon is running')
+            evloop = asyncio.new_event_loop()
+            evloop.run_until_complete(self._run_async())
+            LOGGER.info('inquest daemon closed')
+        except Exception:
+            # when the runner fails out we set enabled to false again
+            with _LOCK:
+                _ENABLED = False
 
     async def _run_async(self):
         url = f'ws://{self.endpoint}/api/graphql'
 
+        # checks that the versions match between the backend and the frontend
         await check_version(f'http://{self.endpoint}/api/version')
 
         consumers = self.client_consumers()
@@ -96,31 +110,39 @@ class ProbeRunner(threading.Thread):
 
 # TODO pass the TraceSet Key in as an argument
 def enable(
-    *,
-    api_key: str,
-    host: str = "inquest.dev",
-    port: int = 80,
-    glob: Optional[Union[str, List[str]]] = None,
-    daemon: bool = True,
-    package: Optional[str] = None,
-    exclude: Optional[List[str]] = None,
+        *,
+        api_key: str,
+        host: str = "inquest.dev",
+        port: int = 80,
+        glob: Optional[Union[str, List[str]]] = None,
+        daemon: bool = True,
+        package: Optional[str] = None,
+        exclude: Optional[List[str]] = None,
+        force_start: bool = False,
 ) -> None:
     '''
     runs the probe in a separate thread
     '''
 
-    if package is None:
-        frame = inspect.stack()[1]
-        mod = inspect.getmodule(frame[0])
-        package = mod.__name__
-    probe = ProbeRunner(
-        package=package,
-        trace_set_id=api_key,
-        host=host,
-        port=port,
-        glob=glob,
-        exclude=exclude,
-    )
-    probe.setName('inquest probe')
-    probe.setDaemon(daemon)
-    probe.start()
+    # _ENABLED & _LOCK to prevent inquest daemon from being fired
+    # up more than once in a given python thread
+    global _ENABLED
+    with _LOCK:
+        if _ENABLED and not force_start:
+            return
+        if package is None:
+            frame = inspect.stack()[1]
+            mod = inspect.getmodule(frame[0])
+            package = mod.__name__
+        probe = ProbeRunner(
+            package=package,
+            trace_set_id=api_key,
+            host=host,
+            port=port,
+            glob=glob,
+            exclude=exclude,
+        )
+        probe.setName('inquest probe')
+        probe.setDaemon(daemon)
+        probe.start()
+        _ENABLED = True
